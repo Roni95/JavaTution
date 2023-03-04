44a3813e91ef383c66f78fcdd71f5b75afba648f
Md. Abid Hasan Roni Bokshi <57047094+Roni95@users.noreply.github.com>
2023-03-04 22:38:03 +0600

    Create LectureOne.java

---
+++ LectureOne.java
@@ -0,0 +1,71 @@
+package classa;
+import java.util.Scanner;
+
+/*
+    char - character ( A-Z, a-z, ! @ $ % + - )
+    String - string is a collection of character 
+    int - integer 
+    float - floating . 6 digit
+    double - floating . 14 digit
+
+    Rules for variable declaration:
+        1. must be start with A-Z, a-z, _ 
+        2. only can use A-Z, a-z, _ , 0-9 
+        3. keyword can't use as a varibale
+        4. white space and special character not allow
+
+    Operator : 
+        1. Arithmetic Operator ( + , - , * , / , % )
+        2. Assignement Operator ( = , += , -= , *= ) 
+        
+    ASCII - American Standard Coded for Information Interchange
+*/
+
+public class ClassA {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        /*
+        char ch='A';
+        System.out.println((int)ch);
+        int roll = 36;
+        System.out.println((char)roll);
+        
+        
+        /*
+        System.out.print("Enter two numbers : ");
+        int number_one = input.nextInt();
+        int number_two = input.nextInt();
+        System.out.println("Summation is : "+(number_one+number_two));
+        System.out.println("Substraction is : "+(number_one-number_two));
+        System.out.println("Multiplication is : "+(number_one*number_two));
+        System.out.println("Division is : "+((double)number_one/number_two));
+        System.out.println("Modulo is : "+(number_one%number_two));
+        
+        /*
+        int roll;
+        System.out.println("Enter your roll : ");
+        roll = input.nextInt();
+        System.out.println("Your roll is : "+roll);
+        double balance;
+        balance = input.nextDouble();
+        System.out.println("Balance is :"+balance);
+        String name;
+        System.out.println("Enter your name : ");
+        input.nextLine();
+        name = input.nextLine();
+        System.out.println("Your name is : "+name);
+        /*
+        int roll = 20;
+        double balance = 5.67;
+        String name= "Roni";
+        char ch = 'A'; 
+        System.out.println("Your roll no is : "+roll);
+        System.out.println("Your name is : "+ name );
+        System.out.println("your balance is : "+ balance);
+        */
+        
+        
+    }
+    
+}
