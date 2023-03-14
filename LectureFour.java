8fa6dfa4963d3a68f656f57e24ba43b2d087e4e6
Md. Abid Hasan Roni Bokshi <57047094+Roni95@users.noreply.github.com>
2023-03-15 01:24:46 +0600

    Create LectureFour.java

---
+++ LectureFour.java
@@ -0,0 +1,48 @@
+package classd;
+public class ClassD {
+    
+    /*
+        What is the difference between JDK, JRE, and JVM?
+        JVM
+        JVM is an acronym for Java Virtual Machine; 
+        it is an abstract machine which provides the runtime environment in which Java bytecode can be executed. It is a specification which specifies the working of Java Virtual Machine. Its implementation has been provided by Oracle and other companies. Its implementation is known as JRE
+        JVMs are available for many hardware and software platforms (so JVM is platform dependent). It is a runtime instance which is created when we run the Java class. There are three notions of the JVM: specification, implementation, and instance.
+
+        JRE
+        JRE stands for Java Runtime Environment. 
+        It is the implementation of JVM. 
+        The Java Runtime Environment is a set of software tools which are used for developing Java applications. It is used to provide the runtime environment. It is the implementation of JVM. It physically exists. It contains a set of libraries + other files that JVM uses at runtime.
+
+        JDK
+        JDK is an acronym for Java Development Kit. 
+        It is a software development environment which is used to develop Java applications and applets. It physically exists. It contains JRE + development tools. JDK is an implementation of any one of the below given Java Platforms released by Oracle Corporation:
+
+        Standard Edition Java Platform
+        Enterprise Edition Java Platform
+        Micro Edition Java Platform
+    */
+
+    public static void main(String[] args) {
+        
+        /*
+        // Which one you will use to prefer while loop or do while loop 
+        int j=1;
+        do{
+            System.out.println("Bangla");
+            j++;
+        } while(j<5);
+        /*
+        int i=1;
+        while(i<5){
+            System.out.println("Bangla");
+            i++;
+        }
+        
+        for(int k=1;k<5 ;k++){
+            System.out.println("Bangladesh");
+        }
+        */
+        //initialization , condition , increment or decrement 
+    }
+}
+
