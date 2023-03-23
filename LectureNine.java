540e7d3876c401495b84323b70f1ef9468d11723
Md. Abid Hasan Roni Bokshi <57047094+Roni95@users.noreply.github.com>
2023-03-24 00:41:43 +0600

    Create LectureNine.java

---
+++ LectureNine.java
@@ -0,0 +1,72 @@
+package classi;
+    // String is collection of character... 
+    //
+public class ClassI {
+    public static void main(String[] args) {
+        
+        /*
+        String test = "World Hello"; //
+        String ans1 = "";
+        String ans2 = "";
+        String ans;
+        char ch;
+        int status = 0;
+        for(int i =0; i<test.length();i++){
+            ch = test.charAt(i);
+            if(ch == ' '){
+                status =1;
+            }
+            if(status == 0){
+                ans1 = ans1 + ch;
+            }
+            else {
+                ans2 = ans2 + ch;
+            }
+        }
+        ans = ans2 + " "+ans1;
+        System.out.println(ans);
+        /*
+        
+        String MainString ="Hello Programmer";
+        int len= MainString.length();
+        System.out.println("Size is : " +len);
+        /*
+        for(int i=0; MainString.charAt(i) !='\0'; i++){
+            len++;
+        }
+        System.out.println("Size is : " +len);
+        
+        /*
+        
+        String MainString ="Hello Programmer";
+        String CopyString="";
+        for(int i=0; i<MainString.length() ;i++){
+            CopyString = CopyString+ MainString.charAt(i);
+        }
+        System.out.println(CopyString);
+        
+        /*
+        CopyString = MainString;
+        System.out.println(CopyString);
+        
+        
+        /*
+        String Name="World Hello";
+        char ch;
+        String Rev=" ";
+        // Revers is lekiM
+        for(int i=0;i< Name.length(); i++){
+            ch = Name.charAt(i);  // k
+            Rev = ch + Rev;       // M+" "= M // i + M = iM // k + iM // lekiM
+        }
+        System.out.println(Rev);
+        
+        /*
+        String first_name = "Abid";
+        String last_name = "Hasan";
+        String full_name = first_name + last_name;
+        System.out.println(full_name);
+        */
+    }
+    
+}
